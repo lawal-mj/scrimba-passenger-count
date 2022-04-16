fb07a011f6388c865ff288ec203e02c24524048a
lawal <lawalmujeeb71@gmail.com>
2022-04-16 21:20:17 +0100

    remove folders

---
+++ index.js
@@ -0,0 +1,10 @@
+// grab the count-el Element, store it ina a count el variable
+
+let countEl = document.getElementById("count-el")
+let count = 0;
+
+console.log(countEl)
+
+function increment() {
+    count++
+}
--- js/index.js
+++
@@ -1,7 +0,0 @@
-let countEl = document.getElementById("count-el");
-let count = 0;
-
-function increment() {
-  count++;
-  countEl.innerText = count;
-}
